07da83b76bf4fc8559ec8a80f5485d634b21a0bc
goelmo <aman.hailu@gmail.com>
2016-11-09 09:17:00 +0300

    html form

--- login.php
+++ login.php
@@ -1,10 +1,27 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Login Page</title>
-  </head>
-  <body>
-    
-  </body>
+<head>
+    <title></title>
+</head>
+<body>
+    <div class='container'>
+    <label for='username' >UserName*:</label><br/>
+    <input type='text' name='username' id='username' value='' maxlength="50" /><br/>
+    <span id='register_username_errorloc' class='error'></span>
+</div>
+<div class='container' style='height:80px;'>
+    <label for='password' >Password*:</label><br/>
+    <div class='pwdwidgetdiv' id='thepwddiv' ></div>
+    <input type='text' name='username' id='username' value='' maxlength="50" /><br/>
+    <noscript>
+    <input type='password' name='password' id='password' maxlength="50" />
+    </noscript>
+    <div id='register_password_errorloc' class='error' style='clear:both'></div>
+</div>
+
+<div class='container'>
+    <input type='submit' name='Submit' value='Submit' />
+</div>
+
+</body>
 </html>
