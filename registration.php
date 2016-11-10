946a960cf5598e60252819d8147fbf1676144cfa
goelmo <aman.hailu@gmail.com>
2016-11-10 10:29:33 +0300

    merhiu help - kickasss

---
+++ registration.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>test</title>
+  </head>
+  <body>
+    <form class="w3school" action="index.html" method="post">
+      Name  : <input type="text " name="name" value="">
+      Password : <input type="text" name="email" value="">
+
+      <input type="submit" value="">
+    </form>
+  </body>
+</html>
