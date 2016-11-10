92053ac8f61496af2b68ea22d2fc793c3423d719
abebe kebede <abebet@yahoo.com>
2016-11-10 02:38:38 -0800

    create a registration_form

--- register.php
+++ register.php
@@ -1,48 +1,23 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>user registration form validation using javascript with example</title>
-</head>
-<body>
-<div id="emptyDiv">
+ <?php
+    $name = isset($_POST['name']) ? $_POST['name'] : '';
+    $lastName = isset($_POST['name']) ? $_POST['last_name'] : '';
+    $email = isset($_POST['name']) ? $_POST['email'] : '';
+    $password = isset($_POST['name']) ? $_POST['password'] : '';
+     
+    
 
-<div id="description"></div>
-<!--container start-->
-<div id="container">
-  <div id="container_body">
-    <div>
-      <h2 class="form_title">User Registration Form </h2>
-      <p class="head_para">Fill all the texts to register</p>
-    </div>
-    <!--Form  start-->
-    <div id="form_name">
-      <div class="firstnameorlastname">
-       <form name="form" action="insert.php" method="post">
-       <div id="errorBox"></div>
-        <input type="text" name="Name" value="" placeholder="First Name"  class="input_name" >
-        <input type="text" name="LastName" value="" placeholder="Last Name" class="input_name" >
-        
-      </div>
-      <div id="email_form">
-        <input type="text" name="Email" value="" placeholder="Your Email" class="input_email">
-      </div>
-      <div id="Re_email_form">
-        <input type="text" name="enterEmail" value="" placeholder="Re-enter Email" class="input_Re_email">
-      </div>
-      <div id="password_form">
-        <input type="password" name="Password" value="" placeholder="New Password" class="input_password">
-      </div>
-      
-       <div>
-        <input type="Submit" value="Register" onClick="insert.php">
-		
-      </div>
-     </form>
-    </div>
-    <!--form ends--> 
-  </div>
-</div>
-<!--container ends-->
-</body>
-</html>
+    $conn = mysql_connect('localhost', 'betty', '123', 'register'); 
+     
+    $query = "INSERT INTO registration ( name, last_name, email, password )
+    VALUES ( '$name', '$lastName', '$email', '$password' );";
+    
+    $result = mysql_query($query);
+     
+    mysql_close();
+
+    if($result) {
+        header("Location:login.php");
+     } else {
+        die("Could not register ");
+     }
+?>
---
+++ registration_form.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>user registration form validation using javascript with example</title>
+</head>
+<body>
+<div id="emptyDiv">
+
+<div id="description"></div>
+<!--container start-->
+<div id="container">
+  <div id="container_body">
+    <div>
+      <h2 class="form_title">User Registration Form </h2>
+      <p class="head_para">Fill all the texts to register</p>
+    </div>
+    <!--Form  start-->
+    <div id="form_name">
+      <div class="firstnameorlastname">
+       <form name="form" action="register.php" method="post">
+       <div id="errorBox"></div>
+        <input type="text" name="name" value="" placeholder="First Name"  class="input_name" >
+        <input type="text" name="last_name" value="" placeholder="Last Name" class="input_name" >
+        
+      </div>
+      <div id="email">
+        <input type="email" name="email" value="" placeholder="Your Email" class="input_email">
+      </div>
+      <div id="Re_email_form">
+        <input type="text" name="confirm_email" value="" placeholder="Re-enter Email" class="input_Re_email">
+      </div>
+      <div id="password_form">
+        <input type="password" name="password" value="" placeholder="New Password" class="input_password">
+      </div>
+      
+       <div>
+        <input type="Submit" name="register_button" value="Register" onClick="register.php">
+		
+      </div>
+     </form>
+    </div>
+    <!--form ends--> 
+  </div>
+</div>
+<!--container ends-->
+</body>
+</html>
